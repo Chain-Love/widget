95c8a076e9d3ddb2f083a689e086bd27000525d4
YanLozovskyi <118999406+YanLozovskyi@users.noreply.github.com>
2025-11-25 16:20:47 +0200

    refactor: remove custom headers configuration from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          // Do not use X-Frame-Options: DENY/SAMEORIGIN
-          {
-            key: "Content-Security-Policy",
-            value:
-              "frame-ancestors 'self' https://*.gitbook.com https://*.gitbook.io",
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig = {};
+
 module.exports = nextConfig;
